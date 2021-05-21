3235946213a2791cba33fe2d7b84bba453351910
Komla Adzam <adzamkomla.dev@gmail.com>
2021-05-21 16:05:43 +0000

    add animations to frontend

--- ng-auth-app/src/app/auth/auth.component.ts
+++ ng-auth-app/src/app/auth/auth.component.ts
@@ -1,25 +1,250 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  Inject,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { Router } from '@angular/router';
-import { AuthService } from '@auth0/auth0-angular';
-import { Observable } from 'rxjs';
+import { DOCUMENT } from '@angular/common';
 
-import {tap} from 'rxjs/operators';
+import { AuthService } from '@auth0/auth0-angular';
+
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
 
 @Component({
   templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.scss']
+  styleUrls: ['./auth.component.scss'],
 })
 export class AuthComponent implements OnInit {
+  @ViewChild('menuLeft', { static: true })
+  menuLeft!: ElementRef<HTMLDivElement>;
+
+  @ViewChild('menuRight', { static: true })
+  menuRight!: ElementRef<HTMLDivElement>;
+
+  @ViewChild('imageFirst', { static: true })
+  imageFirst!: ElementRef<HTMLImageElement>;
+
+  @ViewChild('imageSecond', { static: true })
+  imageSecond!: ElementRef<HTMLImageElement>;
+
+  @ViewChild('secondSection', { static: true })
+  secondSection!: ElementRef<HTMLDivElement>;
+
   isAuthenticated$!: Observable<boolean>;
 
-  constructor(private readonly router: Router,private readonly authService: AuthService) { }
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    private readonly router: Router,
+    private readonly authService: AuthService
+  ) {}
 
   ngOnInit(): void {
-    this.isAuthenticated$ = this.authService.isAuthenticated$.pipe(tap((isAuthenticated) => {
-      if (isAuthenticated) {
-        this.router.navigateByUrl('home');
-      }
-    }));
+    this.isAuthenticated$ = this.authService.isAuthenticated$.pipe(
+      tap((isAuthenticated) => {
+        if (isAuthenticated) {
+          this.router.navigateByUrl('home');
+        }
+      })
+    );
+
+    this.initialAnimations();
+    this.initScrollAnimations();
   }
 
+  initScrollAnimations(): void {
+    gsap.to(this.imageFirst.nativeElement, {
+      scrollTrigger: {
+        trigger: this.imageFirst.nativeElement,
+        scrub: true,
+
+        start: '110% center',
+      } as gsap.plugins.ScrollTriggerInstanceVars,
+      duration: 1.1,
+      scale: 1.2,
+      height: 250,
+    });
+    gsap.to(this.imageSecond.nativeElement, {
+      scrollTrigger: {
+        trigger: this.imageSecond.nativeElement,
+        scrub: true,
+        start: '80% center',
+      } as gsap.plugins.ScrollTriggerInstanceVars,
+      duration: 1.1,
+      scale: 1.2,
+      height: 380,
+    });
+
+    gsap.to(this.document.querySelector('.heading-1'), {
+      scrollTrigger: {
+        trigger: this.document.querySelector('.heading-1'),
+        scrub: true,
+        start: '150% center',
+      } as gsap.plugins.ScrollTriggerInstanceVars,
+      color: '#fff',
+      duration: 1.5,
+    });
+    gsap.to(this.document.querySelector('.paragraph'), {
+      scrollTrigger: {
+        trigger: this.document.querySelector('.paragraph'),
+        scrub: true,
+        start: '150% center',
+      } as gsap.plugins.ScrollTriggerInstanceVars,
+      color: '#fff',
+      duration: 1.5,
+    });
+    gsap.to(this.document.querySelector('.btn'), {
+      scrollTrigger: {
+        trigger: this.document.querySelector('.btn'),
+        scrub: true,
+        start: '150% center',
+      } as gsap.plugins.ScrollTriggerInstanceVars,
+      color: '#fff',
+      duration: 1.5,
+    });
+
+    gsap.from(this.document.querySelector('#about'), {
+      scrollTrigger: {
+        trigger: this.document.querySelector('#about'),
+        scrub: true,
+        toggleClass: 'active',
+        start: 'top center',
+      } as gsap.plugins.ScrollTriggerInstanceVars,
+      duration: 1.5,
+      y: 40,
+      opacity: 0,
+    });
+    gsap.from(this.document.querySelector('#buy'), {
+      scrollTrigger: {
+        trigger: this.document.querySelector('#buy'),
+        scrub: true,
+        toggleClass: 'active',
+        start: 'top center',
+      } as gsap.plugins.ScrollTriggerInstanceVars,
+      duration: 1.5,
+      y: 40,
+      opacity: 0,
+    });
+    gsap.from(this.document.querySelector('.box'), {
+      scrollTrigger: {
+        trigger: this.document.querySelector('.box'),
+        scrub: true,
+        toggleClass: 'active',
+        start: '-10% center',
+      } as gsap.plugins.ScrollTriggerInstanceVars,
+      duration: 1.5,
+      width: 0,
+      opacity: 0,
+    });
+    gsap.from(this.document.querySelector('.info-1__visual img'), {
+      scrollTrigger: {
+        trigger: this.document.querySelector('.info-1__visual img'),
+        scrub: true,
+        toggleClass: 'active',
+        start: '-60% bottom',
+      } as gsap.plugins.ScrollTriggerInstanceVars,
+      duration: 1.5,
+      height: 0,
+      scale: '1.3',
+      opacity: 0,
+    });
+
+    gsap.from(this.document.querySelector('.quote'), {
+      scrollTrigger: {
+        trigger: this.document.querySelector('.quote'),
+        scrub: true,
+        toggleClass: 'active',
+        start: '-60% bottom',
+      } as gsap.plugins.ScrollTriggerInstanceVars,
+      duration: 1.5,
+      opacity: 0,
+    });
+    gsap.from(this.document.querySelector('.info-1__text .heading-3'), {
+      scrollTrigger: {
+        trigger: this.document.querySelector('.info-1__text .heading-3'),
+        scrub: true,
+        toggleClass: 'active',
+        start: '-60% bottom',
+      } as gsap.plugins.ScrollTriggerInstanceVars,
+      duration: 1.5,
+      y: 40,
+      opacity: 0,
+    });
+    gsap.from(this.document.querySelector('.info-1__text .paragraph'), {
+      scrollTrigger: {
+        trigger: this.document.querySelector('.info-1__text .paragraph'),
+        scrub: true,
+        toggleClass: 'active',
+        start: '-60% bottom',
+      } as gsap.plugins.ScrollTriggerInstanceVars,
+      duration: 1.5,
+      y: 40,
+      opacity: 0,
+    });
+    gsap.from(this.document.querySelector('.info-1__text .btn--learn'), {
+      scrollTrigger: {
+        trigger: this.document.querySelector('.info-1__text .btn--learn'),
+        scrub: true,
+        toggleClass: 'active',
+        start: '-60% bottom',
+      } as gsap.plugins.ScrollTriggerInstanceVars,
+      duration: 1.5,
+      y: 40,
+      opacity: 0,
+    });
+  }
+
+  initialAnimations(): void {
+    gsap.from(this.menuLeft.nativeElement.childNodes, {
+      duration: 0.5,
+      opacity: 0,
+      y: -20,
+      stagger: 0.2,
+      delay: 0.5,
+    });
+    gsap.from(this.menuRight.nativeElement.childNodes, {
+      duration: 0.5,
+      opacity: 0,
+      y: -20,
+      stagger: 0.2,
+      delay: 0.8,
+    });
+    gsap.from(this.imageFirst.nativeElement, {
+      duration: 0.7,
+      opacity: 0,
+      y: -30,
+      delay: 0.5,
+    });
+    gsap.from(this.imageSecond.nativeElement, {
+      duration: 0.7,
+      opacity: 0,
+      y: -30,
+      delay: 0.6,
+    });
+    gsap.from(this.document.querySelector('.heading-1'), {
+      duration: 0.7,
+      opacity: 0,
+      y: -30,
+      delay: 0.6,
+    });
+    gsap.from(this.document.querySelector('.paragraph'), {
+      duration: 0.7,
+      opacity: 0,
+      y: -30,
+      delay: 0.7,
+    });
+    gsap.from(this.document.querySelector('.btn'), {
+      duration: 0.7,
+      opacity: 0,
+      y: -30,
+      delay: 0.8,
+    });
+  }
 }
